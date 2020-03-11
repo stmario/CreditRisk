5a0265f97663206345dc38cf48d26a41e567eec8
mariostockli <stoeckli.mario@gmail.com>
2020-03-11 22:01:18 +0100

    Added input, read with pandas

--- CreditRisk.py
+++ CreditRisk.py
@@ -2,3 +2,8 @@ import pandas as pd
 import sklearn as skl
 import numpy as np
 
+correlation = pd.read_csv('Correlation.csv')
+factor_loadings = pd.read_csv('Factor_Loadings.csv')
+pd_table = pd.read_csv('PD_Table.csv')
+portfolio = pd.read_csv('Portfolio.csv')
+
